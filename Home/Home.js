d449a174cb705d7c611fe851228d9756876422ab
iClassic-Live <1227889511@qq.com>
2018-02-07 00:24:41 +0800

    MultiNote 1.7.4

--- Home/Home.js
+++ Home/Home.js
@@ -1,29 +1,24 @@
-const app = getApp();
-
 Page({
   data: {
     motto: "Make Noting Multiply",
     startBtn: "开始使用"
   },
 
-  onLoad: function (e) {
+  onLoad (res) {
     console.log("Home onLoad");
-  },
-  onShow: function (e) {
-    console.log("Home onShow");
   },
 
 /* 自定义用户交互逻辑 */
-    //MultiNote开始使用按钮，当有记事时则跳转到读记事页，否则跳转到写记事页，并检测用户是否已获取微信号的头像昵称
-    toNote: function () {
-      if (wx.getStorageSync("note").length > 0) {
-        wx.redirectTo({
-          url: "../ShowNote/ShowNote"
-        });
-      } else {
-        wx.redirectTo({
-          url: "../CreateNote/CreateNote"
-        });
-      }
-    },
+  //MultiNote开始使用按钮，当用户缓存中有记事时则跳转到读记事页，否则跳转到写记事页
+  startUsing (res) {
+    if (wx.getStorageSync("note").length > 0) {
+      wx.redirectTo({
+        url: "../ShowNote/ShowNote"
+      });
+    } else {
+      wx.redirectTo({
+        url: "../CreateNote/CreateNote"
+      });
+    }
+  },
 })
